a8e84aa911b7c17a33084ebc1e08068b21fea5ba
Andrew DeChamplain <andrewdechamplain@cmail.carleton.ca>
2014-01-28 15:30:08 -0500

    Update client.c

--- Assignment1/Assign1a/client.c
+++ Assignment1/Assign1a/client.c
@@ -34,9 +34,7 @@ int main(int argc, char *argv[])
    	hp = gethostbyname(argv[1]);	// check if valid port
    	if (hp==0) error("Unknown host");
 
-   	bcopy((char *)hp->h_addr, 
-   	     	(char *)&server.sin_addr,
-   	      	hp->h_length);
+   	bcopy((char *)hp->h_addr, (char *)&server.sin_addr, hp->h_length);
    	server.sin_port = htons(atoi(argv[2]));
    	length=sizeof(struct sockaddr_in);
 	
