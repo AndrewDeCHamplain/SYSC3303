22e517583729b5cbd2cf31793f2aa0d764efc201
Andrew DeChamplain <andrewdechamplain@cmail.carleton.ca>
2014-02-09 22:09:48 -0500

    Update Executor.java

--- Assignment2/PartA/Executor.java
+++ Assignment2/PartA/Executor.java
@@ -2,15 +2,41 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 public class Executor {
 	
-	public static void execute() throws InterruptedException, ExecutionException {
-		//creates cached thread pool
-				ExecutorService worker = Executors.newCachedThreadPool();
+	public static void execute(int jobs) throws InterruptedException, ExecutionException {
+		int[][] workersInfo = new int[jobs][3];
+		
+		for(int i=0;i<jobs;i++){
+			int workTime;
+			workTime = randomInt(1000,10000);
+				
+			// first column is the job #
+			workersInfo[i][0] = i;
+			// second column is the expected execution time
+			workersInfo[i][1] = workTime;
+			
+			System.out.println("Worker"+workersInfo[i][0] + " started");	// for testing purposes
+					
+			//creates cached thread pool
+			ExecutorService worker = Executors.newCachedThreadPool();
 		        
-		      //Callable thread starts to execute
-				Future<Integer> future=worker.submit(new CallableThread());
+			//Callable thread starts to execute
+			Future<Integer> future=worker.submit(new CallableThread(workTime));		
+			
+			//get workers actual execution times
+			workersInfo[i][2] = future.get();
+		}
+		for (int i = 0; i<jobs; i++){
+		    System.out.println("Worker"+workersInfo[i][0] + ", Expected: " + workersInfo[i][1] +" Actual: " +workersInfo[i][2]);
+		}
+	}
+	
+	// creates random number between a min and max value
+	public static int randomInt(int min, int max)
+	{
+	   int range = (max - min) + 1;     
+	   return (int)(Math.random() * range) + min;
 	}
 }
