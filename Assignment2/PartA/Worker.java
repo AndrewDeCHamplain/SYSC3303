9707e86502d4008ca253b89ec00f04897c617254
Andrew DeChamplain <andrewdechamplain@cmail.carleton.ca>
2014-02-09 22:01:02 -0500

    Update Worker.java

--- Assignment2/PartA/Worker.java
+++ Assignment2/PartA/Worker.java
@@ -1,3 +1,27 @@
 public class Worker {
 	
+	public static int exeTime;
+
+	public static int execute(int workTime) {
+		// start execution timer
+		final long startTime = System.currentTimeMillis();
+		
+		//worker working
+		Log.addWorker();
+		
+		// doing "work" that takes a random amount of time
+		try {
+			Thread.sleep(workTime);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// worker done working
+		Log.removeWorker();
+		
+		//get execution time
+		final long endTime = System.currentTimeMillis();
+		return exeTime = (int) (endTime-startTime);
+	}
 }
