fa54457391849c880a68b210e79d0dff08fb9c8a
Andrew DeChamplain <andrewdechamplain@cmail.carleton.ca>
2014-02-11 00:00:10 -0500

    Update Executor.java

--- Assignment2/PartA/Executor.java
+++ Assignment2/PartA/Executor.java
@@ -1,13 +1,20 @@
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class Executor {
 	
 	public static void execute(int jobs) throws InterruptedException, ExecutionException {
 		int[][] workersInfo = new int[jobs][3];
 		Object future[] = new Object[jobs];
+		
+		//creates cached thread pool
+		ExecutorService worker = Executors.newCachedThreadPool();
+		((ThreadPoolExecutor) worker).setKeepAliveTime(10, TimeUnit.SECONDS); 
+		System.out.println("Number of active threads before execution: "+((ThreadPoolExecutor) worker).getActiveCount()); 
 		
 		for(int i=0;i<jobs;i++){
 			int workTime;
@@ -19,26 +26,33 @@ public class Executor {
 			workersInfo[i][1] = workTime;
 			
 			System.out.println("Worker"+workersInfo[i][0] + " started");	// for testing purposes
-					
-			//creates cached thread pool
-			ExecutorService worker = Executors.newCachedThreadPool();
-		        
+			
 			//Callable thread starts to execute
 			future[i]=worker.submit(new CallableThread(workTime));		
-			
-
 		}
+		System.out.println("Number of active threads: "+((ThreadPoolExecutor) worker).getActiveCount());
 		for (int i = 0; i<jobs; i++){
 			//get workers actual execution times
 			Object obj = future[i];
 			try { 
 				if (obj instanceof Future) {
+					@SuppressWarnings({ "rawtypes", "unchecked" })
 					Future f = (Future<Integer>)obj; 
 					workersInfo[i][2] = (int) f.get();
 					System.out.println("Worker"+workersInfo[i][0] + ", Expected: " + workersInfo[i][1] +" Actual: " +workersInfo[i][2]);
 				}
 			} catch (Exception e) { }
 		}
+		System.out.println("\nNumber of active threads after execution: "+((ThreadPoolExecutor) worker).getActiveCount());
+		System.out.println("Number of cached threads currently in pool: "+((ThreadPoolExecutor) worker).getPoolSize());
+		System.out.println("\nWaiting 10 seconds for idle workers to be decommissioned...\n");
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		System.out.println("Number of cached threads currently in pool: "+((ThreadPoolExecutor) worker).getPoolSize());
 	}
 	
 	// creates random number between a min and max value
