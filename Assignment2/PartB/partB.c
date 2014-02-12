8f231d559459b14602db153b0f159ce516998864
Andrew DeChamplain <andrewdechamplain@cmail.carleton.ca>
2014-02-11 22:16:48 -0500

    Update partB.c

--- Assignment2/PartB/partB.c
+++ Assignment2/PartB/partB.c
@@ -7,60 +7,175 @@
 #include <sys/mman.h>
 #include <rtdk.h>
 #include <string.h>
+#include <time.h>
 
 #include <native/task.h>
 #include <native/timer.h>
 #include <native/mutex.h>
 
-RT_TASK fusThread, accThread, gyroThread;
-RT_MUTEX mutex_30, mutex_60, mutex_300;
+#define BUFFERSIZE 900
 
+RT_TASK fusThread, accThread, gyroThread;
+RT_MUTEX mutex_gyro, mutex_acc;
+int isRunning, acc_sample, gyro_sample[BUFFERSIZE], gyroIndex, printlog[20000],logIndex;
 
 /* NOTE: error handling omitted for now. */
 
 void accelerometer(void *arg)
 {
-	rt_mutex_acquire(&mutex_30,TM_INFINITE);
-		// critical section
-	rt_mutex_release(&mutex_30);
+	RTIME now;
+	int acceptable;
+        /*
+         * Arguments: &task (NULL=self), start time, period 
+         */
+        rt_task_set_periodic(NULL, TM_NOW, 33333333);
+
+	while (isRunning) {
+		
+                rt_task_wait_period(NULL);
+		acceptable = 0;
+		//acceptable = rand() % (800 - 0 + 1) + 0; // acceleration between 0-50 
+		acceptable = rand_lim(20);
+		rt_mutex_acquire(&mutex_acc,TM_INFINITE);
+		
+		if(acceptable > acc_sample)
+			acc_sample = acc_sample + 1;
+
+		else if(acceptable < acc_sample)
+			acc_sample = acc_sample - 1;
+		
+		rt_mutex_release(&mutex_acc);
+	}
 }
 
 void gyroscope(void *arg)
 {
-	rt_mutex_acquire(&mutex_300,TM_INFINITE);
-		// critical section
-	rt_mutex_release(&mutex_300);
+	RTIME now;
+	int randG, i, prev;
+	gyroIndex = 0;
+
+        /*
+         * Arguments: &task (NULL=self), start time, period 
+         */
+        rt_task_set_periodic(NULL, TM_NOW, 3333333);
+
+	while (isRunning) {
+		for(i=0; i<BUFFERSIZE; i++){
+                	rt_task_wait_period(NULL);
+			//randG = rand() % (361 - 0 + 1) + 0; // angle between 0-361
+			randG = rand_lim(360);
+			
+			rt_mutex_acquire(&mutex_gyro,TM_INFINITE);
+			//printf("Random: %d\n", randG);
+			prev = gyroIndex;
+			gyroIndex++;
+			
+			if(gyro_sample[prev] >= 360 && randG > gyro_sample[prev])
+				gyro_sample[gyroIndex] = 1;
+
+			else if(gyro_sample[prev] <= 1 && randG < gyro_sample[prev])
+				gyro_sample[gyroIndex] = 360;
+
+			else if(randG > gyro_sample[prev])
+				gyro_sample[gyroIndex] = gyro_sample[prev] + 5;
+
+			else if(randG < gyro_sample[prev])
+				gyro_sample[gyroIndex] = gyro_sample[prev] - 5;
+
+			else 
+				gyro_sample[gyroIndex] = gyro_sample[prev];
+			
+			rt_mutex_release(&mutex_gyro);
+			
+		}
+		
+		rt_mutex_acquire(&mutex_gyro,TM_INFINITE);
+		gyroIndex = 0;
+		rt_mutex_release(&mutex_gyro);
+	}
 }
 
 void sensor_fusion(void *arg)
 {
-	rt_mutex_acquire(&mutex_60,TM_INFINITE);
-		// critical section
-	rt_mutex_release(&mutex_60);
+	RTIME now;
+	int i,sum, print,k, count;
+        /*
+         * Arguments: &task (NULL=self), start time, period 
+         */
+        rt_task_set_periodic(NULL, TM_NOW, 16666666);
+	count =0;
+	while (isRunning) {
+		count++;
+                rt_task_wait_period(NULL);
+		rt_mutex_acquire(&mutex_acc,TM_INFINITE);
+		rt_mutex_acquire(&mutex_gyro,TM_INFINITE);
+		k=gyroIndex;
+		sum=0; print = 0;
+   		for (i=0; i<5; i++){
+	 		sum = sum + gyro_sample[k];
+			k--;
+		}
+
+		print = ((sum/5) + acc_sample)/2;
+
+		rt_mutex_release(&mutex_gyro);
+		rt_mutex_release(&mutex_acc);
+		
+		printlog[logIndex] = print;
+		logIndex++;
+
+		if(count == 50)
+			isRunning = 0;
+	}
+}
+
+int rand_lim(int limit) {
+/* return a random number between 0 and limit inclusive.
+ */
+
+    int divisor = RAND_MAX/(limit+1);
+    int retval;
+
+    do { 
+        retval = rand() / divisor;
+    } while (retval > limit);
+
+    return retval;
 }
 
 void catch_signal(int sig)
 {
-
+	rt_mutex_delete(&mutex_acc);
+	rt_mutex_delete(&mutex_gyro);
 }
 
 int main(int argc, char* argv[])
 {
-        //signal(SIGTERM, catch_signal);
+        signal(SIGTERM, catch_signal);
         //signal(SIGINT, catch_signal);
+	
+	int statusA, statusG, statusF, mutexacc, mutexgyro, i,j;
 
-	int statusA, statusG, statusF, mutex30, mutex60, mutex300;
-
+	logIndex = 0;
 	// enable rt_task_print
 	rt_print_auto_init(1);
 	
         /* Avoids memory swapping for this program */
         mlockall(MCL_CURRENT|MCL_FUTURE);
 
-	mutex30 = rt_mutex_create(&mutex_30,"30Hz");
-	mutex60 = rt_mutex_create(&mutex_60,"60Hz");
-	mutex300 = rt_mutex_create(&mutex_300,"300Hz");
 
+	// Create mutexs
+	mutexgyro = rt_mutex_create(&mutex_gyro,NULL);
+	if(mutexgyro!=0){
+		fprintf(stderr, "Unable to create gyroscope mutex! Exiting...\n");
+      		exit(EXIT_FAILURE);
+	}
+	mutexacc = rt_mutex_create(&mutex_acc,NULL);
+	if(mutexacc!=0){
+		fprintf(stderr, "Unable to create accelorometer mutex! Exiting...\n");
+      		exit(EXIT_FAILURE);
+	}
+	isRunning = 1;
         /*
          * Arguments: &task, name, stack size (0=default), priority,
          *            mode (FPU, start suspended, ...)
@@ -72,19 +187,26 @@ int main(int argc, char* argv[])
         /*
          * Arguments: &task, task function, function argument
          */
+
 	statusA = rt_task_start(&accThread, &accelerometer, NULL);
 	statusG = rt_task_start(&gyroThread, &gyroscope, NULL);
-        statusF = rt_task_start(&fusThread, &sensor_fusion, NULL);
-
-
+	statusF = rt_task_start(&fusThread, &sensor_fusion, NULL);
+	
+	
 	// threads in infinite loop, therefore this prevents the main from finishing
 	// (and killing its threads)
 	statusA = rt_task_join(&gyroThread);
         statusF = rt_task_join(&fusThread);
 	statusG = rt_task_join(&accThread);
+	printlog[logIndex] = 4000;
 	
 	
-	rt_mutex_delete(&mutex_desc);
+	for (i = 0; i < 50; i++) {
+		printf("Fusion%d: %d\n", i, printlog[i]);
+    	}
+
+	rt_mutex_delete(&mutex_acc);
+	rt_mutex_delete(&mutex_gyro);
 
 }
 
